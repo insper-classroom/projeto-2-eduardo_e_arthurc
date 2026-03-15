45c198eef2b808a62ba14c0c748d8c45b612702a
Eduardo Pessoa <eduardocp8@al.insper.edu.br>
2026-03-15 17:31:51 -0300

    adiciona rota para buscar imóvel por ID

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def listar_imoveis():
     return jsonify([]), 200
 
+@app.route("/imoveis/<int:id>", methods=["GET"])
+def buscar_imovel_por_id(id):
+    return jsonify({"id": id}), 200
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
